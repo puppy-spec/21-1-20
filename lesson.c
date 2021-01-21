2b978520a5b1c4bcaf9c45c4382d4da6db4853d6
puppy-spec <2397660983@qq.com>
2021-01-21 19:10:08 +0800

    今日份代码

---
+++ lesson.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	int i = (a > b) ? b : a;
+//	for (; i >= 1; i--)
+//	{
+//		if (a%i == 0 && b%i == 0)
+//		{
+//			printf("’“µΩ¿≤£∫%d\n", i);
+//			break;
+//		}
+//	}
+//	return 0;
+//}
