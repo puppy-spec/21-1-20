1ad71c62c6864fab43fd426c529decab518935b3
puppy-spec <2397660983@qq.com>
2021-01-31 20:18:55 +0800

    水仙花数

---
+++ 1-31.c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <math.h>
+//打印水仙花数
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 10000000; i++)
+//	{
+//		//判断i是否为自幂数
+//		int sum = 0;
+//		//1. 计算i的位数n
+//		int tmp = i;//12
+//		int n = 1;
+//		while (tmp/10)
+//		{
+//			n++;
+//			tmp /= 10;
+//		}
+//		//2. 获取i的每位，并计算每一位的n次方之和
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += (int)pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		//3. 判断并输出
+//		if (sum == i)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+	int b = -10;
+	//原码是原数字按二进制写出
+	//整数存的是补码
+	//正数的原码反码补码相同
+	//
+	return 0;
+}
