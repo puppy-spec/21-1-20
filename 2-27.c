45578f87864df7625d9c1b004bbd84293ea08ac6
puppy-spec <2397660983@qq.com>
2021-02-27 21:52:28 +0800

    操作符

---
+++ 2-27.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//	short s = 3;
+//	int a = 10;
+//	printf("%d\n", sizeof(s = a + 5));//sizeof括号中放的表达式是不计算的
+//	printf("%d\n", s);
+//
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	while (~scanf("%d", &a))
+//	{
+//		int a = 10;
+//		int b = ++a;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	//int b = ++a;
+//	int b = a++;
+//	printf("%d %d\n", a, b);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0, a = 0, b = 2, c = 3, d = 4;
+//	//i = a++ && ++b && d++;对于逻辑与操作符，左边如果为假，右边就不运算了
+//
+//	//i = a++ || ++b || d++;对于逻辑或操作符，左边如果为真，右边就不运算了
+//	printf("%d %d %d %d\n", a, b, c, d);
+//
+//	printf("%d\n",i);
+//	//1 2 3 4 0		&&
+//
+//	return 0;
+//}
+
+int main()
+{
+	int a = 0;
+
+	return 0;
+}
