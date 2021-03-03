da14f820fcccddf3427d77f4ea57c2cd244b3a4e
puppy-spec <2397660983@qq.com>
2021-03-03 21:43:07 +0800

    my_strlen的实现

---
+++ 3-3.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		*(p + i) = i;
+//	}
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		*p++ = i;
+//	}
+//	p = arr;
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	printf("%d\n", &arr[9] - &arr[0]);
+//	//指针减去指针，得到的绝对值是得到的是指针和指针之间的元素个数
+//	char ch[5] = { 0 };
+//	printf("%d\n", &arr[9] - &ch[3]);
+//	//指针-指针，计算的前提条件是：两指针指向同一块内存
+//	return 0;
+//}
+//计数器
+//递归
+//int my_strlen(char* str)
+//{
+//	//int count = 0;
+//	//while (*str != '\0')
+//	//{
+//	//	count++;
+//	//	str++;
+//	//}
+//	//return count;
+//
+//	/*if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;*/
+//
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return str - start;
+//
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//
+//	return 0;
+//}
+
