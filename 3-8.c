73aa1a7882e061c914940ae6bcbb31825ee4b827
puppy-spec <2397660983@qq.com>
2021-03-08 23:01:09 +0800

    复习+指针

---
+++ 3-8.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//九九乘法表
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j < i; j++)
+//		{
+//			printf("%d*%d=%2d ", i, j, i*j);
+//		}
+//		printf("\n");
+//
+//	}
+//	return 0;
+//
+
+
+
+//int main()
+//{
+//	char ch = 'a';
+//	char* pc = &ch;
+//
+//	char* p = "abcdef";//把字符串首地址放在p里
+//	printf("%c\n", *p);
+//	printf("%s\n", p);
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char str1[] = "hello bit.";
+//	char str2[] = "hello bit.";
+//	char *str3 = "hello bit.";
+//	char *str4 = "hello bit.";
+//
+//	if (str1 == str2)
+//		printf("str1 and str2 are same\n");
+//	else 
+//		printf("str1 and str2 are not same\n");
+//
+//	if (str3 == str4)
+//		printf("str3 and str4 are same\n");
+//	else
+//		printf("str3 and str4 are not same\n");
+//
+//	return 0;
+//}
+
+int main()
+{
+
+
+	return 0;
+}
